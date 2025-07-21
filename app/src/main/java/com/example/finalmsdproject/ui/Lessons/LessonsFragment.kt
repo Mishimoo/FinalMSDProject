940b9ec52639897a7ef9ecbf83e1572850e1719f
Bcarter21 <baileycarter21@gmail.com>
2025-07-21 15:04:44 -0400

    Added working fire base login page registration page complete profile page these will lead into main activity added user account added profile picture options and changed main nav to update the name and pic of the current user added learning modules and start here screens

---
+++ app/src/main/java/com/example/finalmsdproject/ui/Lessons/LessonsFragment.kt
@@ -0,0 +1,16 @@
+package com.example.finalmsdproject.ui.Lessons
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalmsdproject.R
+
+class LessonsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_lessons, container, false)
+    }
+}
